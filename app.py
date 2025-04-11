7f5f02bf33ba27062b94b15b55ce3e85810395f8
Charles Hancock <chuck94@nmsu.edu>
2025-04-11 10:07:16 -0600

    commented out print statement from testing

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
     number = None
     if request.method == 'POST':
         number = random.randint(1, 1000)
-        print(f"Generated number: {number}")
+        #print(f"Generated number: {number}")
     return render_template('index.html', number=number)
 
 if __name__ == '__main__':
